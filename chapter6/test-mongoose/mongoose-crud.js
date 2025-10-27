658190c31ff956aba3251854eb8ba3e232362e43
dsmannk <dsmannk@gmail.com>
2025-10-27 23:15:31 +0900

    도커 세팅

--- chapter6/test-mongoose/mongoose-crud.js
+++ chapter6/test-mongoose/mongoose-crud.js
@@ -5,7 +5,7 @@ dotenv.config({path: '../../.env'});
 import mongoose from 'mongoose';
 import Person from './person-model.js';
 
-let uri = process.env.MONGODB_ATLAS_1;
+let uri = process.env.MONGODB_ATLAS;
 
 if (!uri) {
     console.error('❌ MONGODB_ATLAS is undefined. Check your .env path or variable name.');
@@ -22,7 +22,7 @@ if (!u.searchParams.has('w')) u.searchParams.set('w', 'majority');
 
 uri = u.toString();
 
-mongoose.set("stricQuery", false); // 설정해줘야 경고가 뜨지 않음
+mongoose.set("strictQuery", false); // 설정해줘야 경고가 뜨지 않음
 
 const app = express();
 app.use(bodyParser.json()); // HTTP에서 body를 파싱하기 위한 설정
@@ -30,5 +30,43 @@ app.listen(3000, async () => {
    console.log("Server started");
    const mongodbUri = uri;
 
-   mongoose.connect(mongodbUri, { useNewUrlParser: true }).then(console.log("Connected to MongoDB"));
+   mongoose
+       .connect(mongodbUri, { useNewUrlParser: true })
+       .then(console.log("Connected to MongoDB"));
+});
+
+// 모든 person 데이터 출력
+app.get("/person", async (req, res) => {
+    const person = await Person.find({});
+    res.send(person);
+});
+
+// 특정 이메일로 person 찾기
+app.get("/person/:email", async (req, res) => {
+   const person = await Person.findOne({email: req.params.email});
+   res.send(person);
+});
+
+// person 데이터 추가하기
+app.post("/person", async (req, res) => {
+    const person = new Person(req.body);
+    await person.save();
+    res.send(person);
+})
+
+// person 데이터 수정하기
+app.put("/person/:email", async (req, res) => {
+   const person = await Person.findOneAndUpdate(
+       { email: req.params.email },
+       { $set: req.body },
+       { new: true }
+   );
+   console.log(person);
+   res.send(person);
+});
+
+// person 데이터 삭제하기
+app.delete("/person/:email", async (req, res) => {
+   await Person.deleteMany({ email: req.params.email });
+   res.send({ success: true });
 });
--- chapter6/try-mongo/test-crud.js
+++ chapter6/try-mongo/test-crud.js
@@ -1,3 +1,60 @@
+import dotenv from 'dotenv';
+dotenv.config({path: '../../.env'});
+import { MongoClient } from 'mongodb';
 
+let uri = process.env.MONGODB_ATLAS;
 
-const MongoClient =
+if (!uri) {
+    console.error('❌ MONGODB_ATLAS is undefined. Check your .env path or variable name.');
+    process.exit(1);
+}
+
+// 문자열 이어붙이기 대신 URLSearchParams 로 안전 결합
+// Node의 WHATWG URL은 mongodb+srv 스킴도 파싱 가능
+const u = new URL(uri);
+
+// 이미 있으면 덮어쓰지 않고 유지하고 싶다면 아래 set 대신 has 체크
+if (!u.searchParams.has('retryWrites')) u.searchParams.set('retryWrites', 'true');
+if (!u.searchParams.has('w')) u.searchParams.set('w', 'majority');
+
+uri = u.toString();
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+async function main() {
+    try {
+        // 커넥트를 생성하고 연결 시도
+        await client.connect();
+        console.log('MongoDB 접속 성공');
+
+        // test 데이터베이스의 person 컬렉션 가져오기
+        const collection = client.db('test').collection('person');
+
+        // 문서 하나 추가
+        await collection.insertOne({ name: 'Andy', age: 30 });
+        console.log('문서 추가 완료');
+
+        // 문서 찾기
+        const documents = await collection.find({ name: 'Andy'}).toArray();
+        console.log('찾은 문서:', documents);
+
+        // 문서 갱신하기
+        await collection.updateOne({ name: 'Andy' }, { $set: { age: 31 }});
+        console.log('문서 업데이트');
+
+        // 갱신된 문서 확인하기
+        const updatedDocuments = await collection.find({ name: 'Andy'}).toArray();
+        console.log('갱신된 문서 : ', updatedDocuments);
+
+        // 문서 삭제하기
+        //await collection.deleteOne({ name: 'Andy' });
+        //console.log('문서 삭제');
+
+        // 연결 끊기
+        await client.close();
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+main();
